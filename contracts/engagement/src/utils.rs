610efb08e3de563f0d913cb03d87a411ea91a34b
armandocodecr <totovip1464@gmail.com>
2024-10-03 16:52:07 -0600

    chore: remove unnnecessary function

--- contracts/engagement/src/utils.rs
+++ contracts/engagement/src/utils.rs
@@ -1,7 +1,3 @@
-use soroban_sdk::{Bytes, BytesN, Env};
+use soroban_sdk::{BytesN, Env, FromVal, String};
 
-pub fn u128_to_bytes(env: &Env, value: u128) -> Bytes {
-    let bytes_array: [u8; 16] = value.to_be_bytes();
-    let bytes_n: BytesN<16> = BytesN::from_array(env, &bytes_array);
-    bytes_n.into()
-}
+// Here will go future functions
