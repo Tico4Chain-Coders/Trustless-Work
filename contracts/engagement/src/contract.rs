0a32f933af7eb2e23e28fc44733b5f90cc875e41
armandocodecr <totovip1464@gmail.com>
2024-10-03 16:52:52 -0600

    chore: update structure contract

--- contracts/engagement/src/contract.rs
+++ contracts/engagement/src/contract.rs
@@ -1,13 +1,12 @@
 use soroban_sdk::{
-    contract, contractimpl, symbol_short, Address, Bytes, Env, Map, String, Vec
+    contract, contractimpl, Address, Env, String
 };
 use soroban_sdk::token::Client as TokenClient;
 
-use crate::storage::{get_engagement, get_all_engagements};
-use crate::storage_types::{Escrow, Engagement, DataKey, User};
+use crate::storage_types::{Escrow, DataKey, User};
 use crate::events::{
-    engagement_created, escrow_added, escrow_completed, escrow_funded, engagement_cancelled, engagement_completed, 
-    engagement_refunded, engagements_by_address, balance_retrieved_event, allowance_retrieved_event
+    escrow_created, escrow_completed, escrow_funded, escrow_cancelled,
+    escrow_refunded, escrows_by_engagement_id, balance_retrieved_event, allowance_retrieved_event
 };
 
 #[contract]
@@ -17,101 +16,90 @@ pub struct EngagementContract;
 impl EngagementContract {
     pub fn initialize_escrow(
         e: Env,
+        engagement_id: String,
+        issuer: Address,
         service_provider: Address,
-        prices: Vec<u128>,
-        client: Address,
-    ) -> u128 {
-        client.require_auth(); 
+        amount: u128,
+        signer: Address,
+    ) -> String {
+        signer.require_auth(); 
 
-        if prices.is_empty() {
-            panic!("Prices cannot be empty");
+        if amount == 0 {
+            panic!("Prices cannot be zero");
         }
 
-        let contract_key = symbol_short!("pk");
-        let mut engagement_count: u128 = e
-            .storage()
-            .instance()
-            .get(&contract_key)
-            .unwrap_or(0);
-    
-        engagement_count += 1;
-        e.storage().instance().set(&contract_key, &engagement_count);
-        let engagement_id = Bytes::from_slice(&e, &engagement_count.to_be_bytes());
-        let mut escrows: Map<u128, Escrow> = Map::new(&e);
-        for (i, price) in prices.iter().enumerate() {
-            escrows.set(i as u128, Escrow {
-                price: price as u128,
-                amount_paid: 0,
-                completed: false,
-            });
-        }
-        let engagement = Engagement {
-            engagement_id,
-            client: client.clone(),
+        let engagement_id_copy = engagement_id.clone();
+        let escrow = Escrow {
+            engagement_id: engagement_id.clone(),
+            issuer: issuer,
+            signer: signer.clone(),
             service_provider: service_provider.clone(),
-            escrows_count: prices.len() as u128,
-            escrows,
-            completed_escrows: 0,
-            earned_amount: 0,
-            contract_balance: 0,
+            amount: amount,
+            balance: 0,
             cancelled: false,
             completed: false,
         };
         
-        let engagement_key = DataKey::Engagement(Bytes::from_slice(&e, &engagement_count.to_be_bytes()));
-        e.storage().instance().set(&engagement_key, &engagement);
-        engagement_created(&e, engagement_key, client.clone(), service_provider.clone(), prices);
+        e.storage().instance().set(&DataKey::Escrow(engagement_id.clone().into()), &escrow);
+        escrow_created(&e, engagement_id, signer.clone(), service_provider.clone(), amount);
 
-        u128::from_be_bytes(engagement_count.to_be_bytes())
-    }
-
-    pub fn complete_engagement(e: Env, engagement_id: Bytes, client: Address) {
-        let (mut engagement, engagement_key) = get_engagement(&e, engagement_id);
-
-        let invoker = client;
-        if invoker != engagement.client {
-            panic!("Only the client can mark the engagement as completed");
-        }
-
-        if engagement.completed {
-            panic!("Engagement is completed");
-        }
-
-        if engagement.cancelled {
-            panic!("Engagement is cancelled");
-        }
-
-        if engagement.completed_escrows != engagement.escrows_count {
-            panic!("Not all escrows completed");
-        }
-
-        engagement.completed = true;
-        e.storage().instance().set(&engagement_key, &engagement);
-        engagement_completed(&e, engagement_key);
+        engagement_id_copy
     }
     
+    pub fn fund_escrow(e: Env, engagement_id: String, signer: Address, usdc_contract: Address, contract_address: Address) {
+        signer.require_auth();
+    
+        let escrow_key = DataKey::Escrow(engagement_id.clone());
+        let mut escrow: Escrow = e.storage().instance().get(&escrow_key).unwrap();
+    
+        if signer != escrow.signer {
+            panic!("Only the signer can fund escrows");
+        }
+
+        if escrow.balance > 0 {
+            panic!("Escrow already funded");
+        }
+
+        if escrow.balance == escrow.amount {
+            panic!("This escrow has already been fully funded.");
+        }
+
+        let half_price = (escrow.amount / 2) as i128;
+        let usdc_client = TokenClient::new(&e, &usdc_contract);
+
+        usdc_client.approve(&signer, &contract_address, &half_price, &e.ledger().sequence());
+        
+        let allowance = usdc_client.allowance(&signer, &contract_address);
+
+        if allowance < half_price {
+            panic!("Not enough allowance to fund this escrow. Please approve the amount first.");
+        }
+
+        usdc_client.transfer(&signer, &contract_address, &half_price);
+
+        escrow.balance = half_price as u128;
+        e.storage().instance().set(&escrow_key, &escrow);
+        escrow_funded(&e, engagement_id, half_price as u128);
+    }
 
     pub fn complete_escrow(
         e: Env,
-        engagement_id: Bytes,
-        escrow_id: u128,
-        client: Address,
+        engagement_id: String,
+        signer: Address,
         usdc_contract: Address,
         contract_address: Address,
         service_provider: Address
     ) {
-        client.require_auth();
+        signer.require_auth();
     
-        let engagement_key = DataKey::Engagement(engagement_id);
-        let mut engagement: Engagement = e.storage().instance().get(&engagement_key).unwrap();
+        let escrow_key = DataKey::Escrow(engagement_id.clone());
+        let mut escrow: Escrow = e.storage().instance().get(&escrow_key).unwrap();
     
-        if service_provider != engagement.service_provider {
-            panic!("Only the service provider can complete escrows");
+        if signer != escrow.signer {
+            panic!("Only the signer can complete the escrow");
         }
     
-        let mut escrow = engagement.escrows.get(escrow_id).unwrap();
-    
-        if escrow.amount_paid == 0 {
+        if escrow.balance == 0 {
             panic!("Escrow not funded");
         }
     
@@ -119,90 +107,94 @@ impl EngagementContract {
             panic!("Escrow already completed");
         }
     
-        let remaining_price = (escrow.price - escrow.amount_paid) as i128;
-        let full_price = escrow.price;
+        let remaining_price = (escrow.amount - escrow.balance) as i128;
+        let full_price = escrow.amount;
     
         let usdc_client = TokenClient::new(&e, &usdc_contract);
         let expiration_ledger = e.ledger().sequence() + 1000;
 
-        usdc_client.approve(&client, &contract_address, &remaining_price, &expiration_ledger);
+        usdc_client.approve(&signer, &contract_address, &remaining_price, &expiration_ledger);
         usdc_client.transfer(
-            &client,              
+            &signer,              
             &contract_address,
             &remaining_price
         );
 
-        usdc_client.approve(&contract_address, &service_provider, &(escrow.price as i128), &expiration_ledger);
+        usdc_client.approve(&contract_address, &service_provider, &(escrow.amount as i128), &expiration_ledger);
         usdc_client.transfer(
             &contract_address,
             &service_provider,
-            &(escrow.price as i128)
+            &(escrow.amount as i128)
         );
-    
+
         escrow.completed = true;
-        engagement.completed_escrows += 1;
-        engagement.earned_amount += escrow.price;
+        escrow.balance = escrow.amount;
     
-        engagement.escrows.set(escrow_id, escrow);
-        e.storage().instance().set(&engagement_key, &engagement);
-    
-        escrow_completed(&e, engagement_key, escrow_id, full_price);
+        e.storage().instance().set(&escrow_key, &escrow);
+        escrow_completed(&e, engagement_id, full_price);
     }
 
-    pub fn cancel_engagement(e: Env, engagement_id: Bytes, client: Address) {
-        client.require_auth();
-        let (mut engagement, engagement_key) = get_engagement(&e, engagement_id);
+    pub fn cancel_escrow(e: Env, engagement_id: String, signer: Address) {
+        signer.require_auth();
+        let escrow_key = DataKey::Escrow(engagement_id.clone());
+        let mut escrow: Escrow = e.storage().instance().get(&escrow_key).unwrap();
 
-        let invoker = client;
-        if invoker != engagement.client {
+        let invoker = signer;
+        if invoker != escrow.signer {
+            panic!("Only the signer can mark the escrow as completed");
+        }
+
+        if escrow.completed {
+            panic!("The escrow is completed");
+        }
+
+        if escrow.cancelled {
+            panic!("The escrow is cancelled");
+        }
+
+        escrow.cancelled = true;
+
+        e.storage().instance().set(&escrow_key, &escrow);
+        escrow_cancelled(&e, escrow_key);
+    }
+
+    pub fn refund_remaining_funds(e: Env, engagement_id: String, signer: Address, usdc_contract: Address, contract_address: Address) {
+        signer.require_auth();
+
+        let escrow_key = DataKey::Escrow(engagement_id.clone());
+        let escrow: Escrow = e.storage().instance().get(&escrow_key).unwrap();
+        
+        let invoker = signer.clone();
+        if invoker != escrow.signer {
             panic!("Only the client can mark the engagement as completed");
         }
-
-        if engagement.completed {
-            panic!("Engagement is completed");
-        }
-
-        if engagement.cancelled {
+        if escrow.cancelled {
             panic!("Engagement is cancelled");
         }
 
-        engagement.cancelled = true;
+        let usdc_client = TokenClient::new(&e, &usdc_contract);
+        let contract_balance = usdc_client.balance(&contract_address);
 
-        e.storage().instance().set(&engagement_key, &engagement);
-        engagement_cancelled(&e, engagement_key);
+        if  contract_balance == 0 {
+            panic!("The contract has no balance to repay");
+        }
+
+        usdc_client.approve(&signer, &contract_address, &contract_balance, &e.ledger().sequence());
+        usdc_client.transfer(
+            &e.current_contract_address(),
+            &escrow.signer,
+            &(contract_balance as i128) 
+        );
+
+        escrow_refunded(&e, escrow_key, signer.clone(), contract_balance as u128);
     }
 
-    pub fn add_escrow(e: Env, engagement_id: Bytes, prices: Vec<u128>, client: Address) {
-        client.require_auth();
-        let (mut engagement, engagement_key) = get_engagement(&e, engagement_id);
-
-        let invoker = client;
-        if invoker != engagement.client {
-            panic!("Only the client can add escrows");
-        }
-
-        if engagement.completed {
-            panic!("Engagement is completed");
-        }
-
-        if engagement.cancelled {
-            panic!("Engagement is cancelled");
-        }
+    pub fn get_escrow_by_id(e: Env, engagement_id: String) -> Escrow {
+        let escrow_key = DataKey::Escrow(engagement_id.clone());
+        let escrow: Escrow = e.storage().instance().get(&escrow_key).unwrap();
         
-        for (i, price) in prices.iter().enumerate() {
-            let escrow_id = engagement.escrows_count + i as u128;
-
-            engagement.escrows.set(escrow_id, Escrow {
-                price: price,
-                amount_paid: 0,
-                completed: false,
-            });
-
-            escrow_added(&e, &engagement_key, escrow_id, price);
-        }
-
-        engagement.escrows_count += prices.len() as u128;
-        e.storage().instance().set(&engagement_key, &engagement);
+        escrows_by_engagement_id(&e, engagement_id.clone(), escrow.clone());
+        escrow
     }
 
     pub fn approve_amounts(e: Env, from: Address, spender: Address, amount: i128, usdc_token_address: Address ) {
@@ -222,122 +214,6 @@ impl EngagementContract {
         let usdc_token = TokenClient::new(&e, &usdc_token_address);
         let balance = usdc_token.balance(&address);
         balance_retrieved_event(&e, address, usdc_token_address, balance);
-    }
-
-    pub fn fund_escrow(e: Env, engagement_id: Bytes, escrow_id: u128, client: Address, usdc_contract: Address, contract_address: Address) {
-        client.require_auth();
-    
-        let engagement_key = DataKey::Engagement(engagement_id);
-        let mut engagement: Engagement = e.storage().instance().get(&engagement_key).unwrap();
-    
-        if client != engagement.client {
-            panic!("Only the client can fund escrows");
-        }
-    
-        let mut escrow = engagement.escrows.get(escrow_id).unwrap();
-        if escrow.amount_paid > 0 {
-            panic!("Escrow already funded");
-        }
-    
-        let half_price = (escrow.price / 2) as i128;
-        let usdc_client = TokenClient::new(&e, &usdc_contract);
-
-        usdc_client.approve(&client, &contract_address, &half_price, &e.ledger().sequence());
-        
-        let allowance = usdc_client.allowance(&client, &contract_address);
-
-        if allowance < half_price {
-            panic!("Not enough allowance to fund this escrow. Please approve the amount first.");
-        }
-
-    
-        usdc_client.transfer(
-            &client,              
-            &contract_address,
-            &half_price       
-        );
-
-    
-        escrow.amount_paid = half_price as u128;
-        engagement.escrows.set(escrow_id, escrow);
-        e.storage().instance().set(&engagement_key, &engagement);
-    
-        escrow_funded(&e, engagement_key, escrow_id, half_price as u128);
-    }
-
-    pub fn refund_remaining_funds(e: Env, engagement_id: Bytes, escrow_id: u128, client: Address, usdc_contract: Address, contract_address: Address) {
-        client.require_auth();
-        let (engagement, engagement_key) = get_engagement(&e, engagement_id);
-
-        let invoker = client.clone();
-        if invoker != engagement.client {
-            panic!("Only the client can mark the engagement as completed");
-        }
-
-        if !engagement.cancelled {
-            panic!("Engagement is cancelled");
-        }
-
-
-        let mut refundable_amount : i128 = 0;
-        for _i in 0..engagement.escrows_count {
-            let mut escrow = engagement.escrows.get(escrow_id).unwrap(); 
-            
-            if !escrow.completed && escrow.amount_paid > 0 {
-                refundable_amount += escrow.amount_paid as i128;
-                escrow.amount_paid = 0; 
-            }
-        }
-        
-        let usdc_client = TokenClient::new(&e, &usdc_contract);
-        let contract_balance = usdc_client.balance(&contract_address);
-        if  contract_balance == 0 {
-            panic!("The contract has no balance to repay");
-        }
-
-        usdc_client.transfer(
-            &e.current_contract_address(),
-            &engagement.client,
-            &(contract_balance as i128) 
-        );
-
-        engagement_refunded(&e, engagement_key, client.clone(), refundable_amount as u128);
-
-    }
-    
-    pub fn get_engagements_by_provider(e: Env, service_provider: Address, page: u32, limit: u32) -> Vec<Engagement> {
-        let all_engagements: Vec<Engagement> = get_all_engagements(e.clone());
-    
-        let mut result: Vec<Engagement> = Vec::new(&e);
-
-        let start = (page * limit) as usize;
-        let end = start + limit as usize;
-
-        for (i, engagement) in all_engagements.iter().enumerate() {
-            if i >= start && i < end && engagement.service_provider == service_provider {
-                result.push_back(engagement);
-            }
-        }
-        engagements_by_address(&e, service_provider, result.clone());
-        result
-    }
-
-    pub fn get_engagements_by_client(e: Env, client: Address, page: u32, limit: u32) -> Vec<Engagement> {
-        let all_engagements: Vec<Engagement> = get_all_engagements(e.clone());
-
-        let mut result: Vec<Engagement> = Vec::new(&e);
-
-        let start = (page * limit) as usize;
-        let end = start + limit as usize;
-
-        for (i, engagement) in all_engagements.iter().enumerate() {
-            if i >= start && i < end && engagement.client == client {
-                result.push_back(engagement);
-            }
-        }
-    
-        engagements_by_address(&e, client, result.clone());
-        result
     }
       
     pub fn register(e: Env, user_address: Address, name: String, email: String) -> bool {
--- contracts/engagement/src/events.rs
+++ contracts/engagement/src/events.rs
@@ -1,33 +1,28 @@
-use soroban_sdk::{Env, vec, IntoVal, Val, Address, Vec, symbol_short};
-use crate::storage_types::{DataKey, Engagement};
+use soroban_sdk::{Env, vec, IntoVal, Val, Address, String, symbol_short};
+use crate::storage_types::{DataKey, Escrow};
 
-pub (crate) fn engagement_created(e: &Env, engagement_id: DataKey, client: Address, service_provider: Address, prices: Vec<u128>) {
+// ------ Escrows
+
+pub (crate) fn escrow_created(e: &Env, escrow_id: String, client: Address, service_provider: Address, prices: u128) {
     let topics = (symbol_short!("p_created"),);
 
-    let engagement_id_val: Val = engagement_id.into_val(e);
+    let escrow_id_val: Val = escrow_id.into_val(e);
     let client_val: Val = client.into_val(e);
     let service_provider_val: Val = service_provider.into_val(e);
     let prices_val: Val = prices.into_val(e);
 
-    let event_payload = vec![e, engagement_id_val, client_val, service_provider_val, prices_val];
+    let event_payload = vec![e, escrow_id_val, client_val, service_provider_val, prices_val];
     e.events().publish(topics, event_payload);
 }
 
-pub (crate) fn engagement_completed(e: &Env, engagement_id: DataKey) {
-    let topics = (symbol_short!("p_c"),); // c -> completed
-
-    let engagement_id_val: Val = engagement_id.into_val(e);
-    e.events().publish(topics, engagement_id_val);
-}
-
-pub (crate) fn engagement_cancelled(e: &Env, engagement_id: DataKey) {
+pub (crate) fn escrow_cancelled(e: &Env, escrow_id: DataKey) {
     let topics = (symbol_short!("p_cd"),); // cd -> cancelled
 
-    let engagement_id_val: Val = engagement_id.into_val(e);
-    e.events().publish(topics, engagement_id_val);
+    let escrow_id_val: Val = escrow_id.into_val(e);
+    e.events().publish(topics, escrow_id_val);
 }
 
-pub (crate) fn engagement_refunded(e: &Env, engagement_id: DataKey, client: Address, price: u128) {
+pub (crate) fn escrow_refunded(e: &Env, engagement_id: DataKey, client: Address, price: u128) {
     let topics = (symbol_short!("p_rd"),); // rd -> refunded
 
     let engagement_id_val: Val = engagement_id.into_val(e);
@@ -39,49 +34,34 @@ pub (crate) fn engagement_refunded(e: &Env, engagement_id: DataKey, client: Addr
     e.events().publish(topics, event_payload);
 }
 
-pub (crate) fn engagements_by_address(e: &Env, address: Address, engagements: Vec<Engagement>) {
+pub (crate) fn escrows_by_engagement_id(e: &Env, engagement_id: String, escrow: Escrow) {
     let topics = (symbol_short!("p_by_spdr"),);
     
-    let address_val: Val = address.into_val(e);
-    let engagements_val: Val = engagements.into_val(e);
-
-    let event_payload = vec![e, address_val, engagements_val];
-    e.events().publish(topics, event_payload);
-}
-
-
-// ------ Escrows
-
-pub (crate) fn escrow_added(e: &Env, engagement_id: &DataKey, escrow_id: u128, price: u128) {
-    let topics = (symbol_short!("ob_added"),);
-
     let engagement_id_val: Val = engagement_id.into_val(e);
-    let escrow_id_val: Val = escrow_id.into_val(e);
-    let price_val: Val = price.into_val(e);
+    let escrow_val: Val = escrow.into_val(e);
 
-    let event_payload = vec![e, engagement_id_val, escrow_id_val, price_val];
+    let event_payload = vec![e, engagement_id_val, escrow_val];
     e.events().publish(topics, event_payload);
 }
 
-pub (crate) fn escrow_funded(e: &Env, engagement_id: DataKey, escrow_id: u128, half_price: u128) {
+
+pub (crate) fn escrow_funded(e: &Env, engagement_id: String, half_price: u128) {
     let topics = (symbol_short!("ob_funded"),);
 
-    let engagement_id_val: Val = engagement_id.into_val(e);
-    let escrow_id_val: Val = escrow_id.into_val(e);
+    let escrow_id_val: Val = engagement_id.into_val(e);
     let half_price_val: Val = half_price.into_val(e);
 
-    let event_payload = vec![e, engagement_id_val, escrow_id_val, half_price_val];
+    let event_payload = vec![e, escrow_id_val, escrow_id_val, half_price_val];
     e.events().publish(topics, event_payload);
 }
 
-pub (crate) fn escrow_completed(e: &Env, engagement_id: DataKey, escrow_id: u128, full_price: u128) {
+pub (crate) fn escrow_completed(e: &Env, escrow_id: String, full_price: u128) {
     let topics = (symbol_short!("ob_c"),); // c -> completed
 
-    let engagement_id_val: Val = engagement_id.into_val(e);
     let escrow_id_val: Val = escrow_id.into_val(e);
     let full_price_val: Val = full_price.into_val(e);
 
-    let event_payload = vec![e, engagement_id_val, escrow_id_val, full_price_val];
+    let event_payload = vec![e, escrow_id_val, escrow_id_val, full_price_val];
     e.events().publish(topics, event_payload);
 }
 
