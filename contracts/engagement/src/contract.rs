b626ac1bb514ed67ef5dd8aebdce1a76e511726b
caleb-sh <calebloriago@gmail.com>
2024-10-14 18:55:44 -0600

    feat: add deploy function

--- contracts/engagement/src/contract.rs
+++ contracts/engagement/src/contract.rs
@@ -23,21 +23,16 @@ impl EngagementContract {
         init_fn: Symbol,
         init_args: Vec<Val>,
     ) -> (Address, Val) {
-        // Skip authorization if deployer is the current contract.
         if deployer != env.current_contract_address() {
             deployer.require_auth();
         }
 
-        // Deploy the contract using the uploaded Wasm with given hash.
         let deployed_address = env
             .deployer()
             .with_address(deployer, salt)
             .deploy(wasm_hash);
 
-        // Invoke the init function with the given arguments.
         let res: Val = env.invoke_contract(&deployed_address, &init_fn, init_args);
-        // Return the contract ID of the deployed contract and the result of
-        // invoking the init result.
         (deployed_address, res)
     }
 
