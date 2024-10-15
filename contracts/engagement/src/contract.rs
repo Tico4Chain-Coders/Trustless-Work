ba0005846cca562052041f46cae8877d99c7acfd
caleb-sh <calebloriago@gmail.com>
2024-10-14 18:55:26 -0600

    feat: add deploy function

--- contracts/engagement/src/contract.rs
+++ contracts/engagement/src/contract.rs
@@ -1,5 +1,5 @@
 use soroban_sdk::{
-    contract, contractimpl, Address, Env, String
+    contract, contractimpl, Address, Env, String, BytesN, Val, Vec, Symbol
 };
 use soroban_sdk::token::Client as TokenClient;
 
@@ -14,6 +14,34 @@ pub struct EngagementContract;
 
 #[contractimpl]
 impl EngagementContract {
+
+    pub fn deploy(
+        env: Env,
+        deployer: Address,
+        wasm_hash: BytesN<32>,
+        salt: BytesN<32>,
+        init_fn: Symbol,
+        init_args: Vec<Val>,
+    ) -> (Address, Val) {
+        // Skip authorization if deployer is the current contract.
+        if deployer != env.current_contract_address() {
+            deployer.require_auth();
+        }
+
+        // Deploy the contract using the uploaded Wasm with given hash.
+        let deployed_address = env
+            .deployer()
+            .with_address(deployer, salt)
+            .deploy(wasm_hash);
+
+        // Invoke the init function with the given arguments.
+        let res: Val = env.invoke_contract(&deployed_address, &init_fn, init_args);
+        // Return the contract ID of the deployed contract and the result of
+        // invoking the init result.
+        (deployed_address, res)
+    }
+
+
     pub fn initialize_escrow(
         e: Env,
         engagement_id: String,
